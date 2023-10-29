2c9c346a7a91901c05d384f2344f74244c525f64
ywyang236 <ywyang236@gmail.com>
2023-10-29 23:53:33 +0800

    Refactor: Extract user data to a separate file

---
+++ src/data/usersData.js
@@ -0,0 +1,532 @@
+// src/data/usersData.js
+
+export const sampleUsers = [
+    // {
+    //     name: "楊于葳 Vivian",
+    //     photoPath: "gs://book-club-system.appspot.com/user_image/0_ywyang236.jpeg",
+    //     data: {
+    //         1: { notes: 0, comments: 0, exercises: 0, activities: 0 },
+    //         2: { notes: 0, comments: 0, exercises: 0, activities: 0 },
+    //         3: { notes: 0, comments: 0, exercises: 0, activities: 0 },
+    //         4: { notes: 0, comments: 0, exercises: 0, activities: 0 },
+    //         5: { notes: 0, comments: 0, exercises: 0, activities: 0 },
+    //         6: { notes: 0, comments: 0, exercises: 0, activities: 0 },
+    //     }
+    // },
+    // {
+    //     name: "Neal",
+    //     photoPath: "gs://book-club-system.appspot.com/user_image/9_neal_180.jpeg",
+    //     data: {
+    //         1: { notes: 0, comments: 0, exercises: 0, activities: 0 },
+    //         2: { notes: 0, comments: 0, exercises: 0, activities: 0 },
+    //         3: { notes: 0, comments: 0, exercises: 0, activities: 0 },
+    //         4: { notes: 0, comments: 0, exercises: 0, activities: 0 },
+    //         5: { notes: 0, comments: 0, exercises: 0, activities: 0 },
+    //         6: { notes: 0, comments: 0, exercises: 0, activities: 0 },
+    //     }
+    // },
+    {
+        "name": "大谷秀瑜",
+        "photoPath": "gs://book-club-system.appspot.com/user_image/8_ohtani_showyu.jpeg",
+        "data": {
+            "1": {
+                "notes": 5,
+                "comments": 0,
+                "exercises": 4,
+                "activities": 1
+            },
+            "2": {
+                "notes": 0,
+                "comments": 0,
+                "exercises": 0,
+                "activities": 0
+            },
+            "3": {
+                "notes": 0,
+                "comments": 0,
+                "exercises": 0,
+                "activities": 0
+            },
+            "4": {
+                "notes": 0,
+                "comments": 0,
+                "exercises": 0,
+                "activities": 0
+            },
+            "5": {
+                "notes": 0,
+                "comments": 0,
+                "exercises": 0,
+                "activities": 0
+            },
+            "6": {
+                "notes": 0,
+                "comments": 0,
+                "exercises": 0,
+                "activities": 0
+            }
+        }
+    },
+    {
+        "name": "Allen",
+        "photoPath": "gs://book-club-system.appspot.com/user_image/4_yiyixiaoqisi.jpeg",
+        "data": {
+            "1": {
+                "notes": 4,
+                "comments": 0,
+                "exercises": 1,
+                "activities": 1
+            },
+            "2": {
+                "notes": 0,
+                "comments": 0,
+                "exercises": 0,
+                "activities": 0
+            },
+            "3": {
+                "notes": 0,
+                "comments": 0,
+                "exercises": 0,
+                "activities": 0
+            },
+            "4": {
+                "notes": 0,
+                "comments": 0,
+                "exercises": 0,
+                "activities": 0
+            },
+            "5": {
+                "notes": 0,
+                "comments": 0,
+                "exercises": 0,
+                "activities": 0
+            },
+            "6": {
+                "notes": 0,
+                "comments": 0,
+                "exercises": 0,
+                "activities": 0
+            }
+        }
+    },
+    {
+        "name": "LTR",
+        "photoPath": "gs://book-club-system.appspot.com/user_image/7_lark1987.jpeg",
+        "data": {
+            "1": {
+                "notes": 5,
+                "comments": 0,
+                "exercises": 0,
+                "activities": 1
+            },
+            "2": {
+                "notes": 0,
+                "comments": 0,
+                "exercises": 0,
+                "activities": 0
+            },
+            "3": {
+                "notes": 0,
+                "comments": 0,
+                "exercises": 0,
+                "activities": 0
+            },
+            "4": {
+                "notes": 0,
+                "comments": 0,
+                "exercises": 0,
+                "activities": 0
+            },
+            "5": {
+                "notes": 0,
+                "comments": 0,
+                "exercises": 0,
+                "activities": 0
+            },
+            "6": {
+                "notes": 0,
+                "comments": 0,
+                "exercises": 0,
+                "activities": 0
+            }
+        }
+    },
+    {
+        "name": "Lulu",
+        "photoPath": "gs://book-club-system.appspot.com/user_image/1_lulu_cheng.jpeg",
+        "data": {
+            "1": {
+                "notes": 5,
+                "comments": 0,
+                "exercises": 0,
+                "activities": 1
+            },
+            "2": {
+                "notes": 0,
+                "comments": 0,
+                "exercises": 0,
+                "activities": 0
+            },
+            "3": {
+                "notes": 0,
+                "comments": 0,
+                "exercises": 0,
+                "activities": 0
+            },
+            "4": {
+                "notes": 0,
+                "comments": 0,
+                "exercises": 0,
+                "activities": 0
+            },
+            "5": {
+                "notes": 0,
+                "comments": 0,
+                "exercises": 0,
+                "activities": 0
+            },
+            "6": {
+                "notes": 0,
+                "comments": 0,
+                "exercises": 0,
+                "activities": 0
+            }
+        }
+    },
+    {
+        "name": "張 雅婷",
+        "photoPath": "gs://book-club-system.appspot.com/user_image/11_yating.chang.jpeg",
+        "data": {
+            "1": {
+                "notes": 4,
+                "comments": 0,
+                "exercises": 2,
+                "activities": 1
+            },
+            "2": {
+                "notes": 0,
+                "comments": 0,
+                "exercises": 0,
+                "activities": 0
+            },
+            "3": {
+                "notes": 0,
+                "comments": 0,
+                "exercises": 0,
+                "activities": 0
+            },
+            "4": {
+                "notes": 0,
+                "comments": 0,
+                "exercises": 0,
+                "activities": 0
+            },
+            "5": {
+                "notes": 0,
+                "comments": 0,
+                "exercises": 0,
+                "activities": 0
+            },
+            "6": {
+                "notes": 0,
+                "comments": 0,
+                "exercises": 0,
+                "activities": 0
+            }
+        }
+    },
+    {
+        "name": "fish",
+        "photoPath": "gs://book-club-system.appspot.com/user_image/10_yuu0210_.jpeg",
+        "data": {
+            "1": {
+                "notes": 5,
+                "comments": 0,
+                "exercises": 0,
+                "activities": 1
+            },
+            "2": {
+                "notes": 0,
+                "comments": 0,
+                "exercises": 0,
+                "activities": 0
+            },
+            "3": {
+                "notes": 0,
+                "comments": 0,
+                "exercises": 0,
+                "activities": 0
+            },
+            "4": {
+                "notes": 0,
+                "comments": 0,
+                "exercises": 0,
+                "activities": 0
+            },
+            "5": {
+                "notes": 0,
+                "comments": 0,
+                "exercises": 0,
+                "activities": 0
+            },
+            "6": {
+                "notes": 0,
+                "comments": 0,
+                "exercises": 0,
+                "activities": 0
+            }
+        }
+    },
+    {
+        "name": "Wenying",
+        "photoPath": "gs://book-club-system.appspot.com/user_image/5_wenying.jpeg",
+        "data": {
+            "1": {
+                "notes": 5,
+                "comments": 0,
+                "exercises": 0,
+                "activities": 1
+            },
+            "2": {
+                "notes": 0,
+                "comments": 0,
+                "exercises": 0,
+                "activities": 0
+            },
+            "3": {
+                "notes": 0,
+                "comments": 0,
+                "exercises": 0,
+                "activities": 0
+            },
+            "4": {
+                "notes": 0,
+                "comments": 0,
+                "exercises": 0,
+                "activities": 0
+            },
+            "5": {
+                "notes": 0,
+                "comments": 0,
+                "exercises": 0,
+                "activities": 0
+            },
+            "6": {
+                "notes": 0,
+                "comments": 0,
+                "exercises": 0,
+                "activities": 0
+            }
+        }
+    },
+    {
+        "name": "Ezra",
+        "photoPath": "gs://book-club-system.appspot.com/user_image/6_ilbas.jpeg",
+        "data": {
+            "1": {
+                "notes": 5,
+                "comments": 0,
+                "exercises": 3,
+                "activities": 1
+            },
+            "2": {
+                "notes": 0,
+                "comments": 0,
+                "exercises": 0,
+                "activities": 0
+            },
+            "3": {
+                "notes": 0,
+                "comments": 0,
+                "exercises": 0,
+                "activities": 0
+            },
+            "4": {
+                "notes": 0,
+                "comments": 0,
+                "exercises": 0,
+                "activities": 0
+            },
+            "5": {
+                "notes": 0,
+                "comments": 0,
+                "exercises": 0,
+                "activities": 0
+            },
+            "6": {
+                "notes": 0,
+                "comments": 0,
+                "exercises": 0,
+                "activities": 0
+            }
+        }
+    },
+    {
+        "name": "肉魚",
+        "photoPath": "gs://book-club-system.appspot.com/user_image/3_rouyu.jpeg",
+        "data": {
+            "1": {
+                "notes": 5,
+                "comments": 0,
+                "exercises": 0,
+                "activities": 1
+            },
+            "2": {
+                "notes": 0,
+                "comments": 0,
+                "exercises": 0,
+                "activities": 0
+            },
+            "3": {
+                "notes": 0,
+                "comments": 0,
+                "exercises": 0,
+                "activities": 0
+            },
+            "4": {
+                "notes": 0,
+                "comments": 0,
+                "exercises": 0,
+                "activities": 0
+            },
+            "5": {
+                "notes": 0,
+                "comments": 0,
+                "exercises": 0,
+                "activities": 0
+            },
+            "6": {
+                "notes": 0,
+                "comments": 0,
+                "exercises": 0,
+                "activities": 0
+            }
+        }
+    },
+    {
+        "name": "AngelTsui",
+        "photoPath": "gs://book-club-system.appspot.com/user_image/13_angeltsui.jpeg",
+        "data": {
+            "1": {
+                "notes": 5,
+                "comments": 0,
+                "exercises": 1,
+                "activities": 1
+            },
+            "2": {
+                "notes": 0,
+                "comments": 0,
+                "exercises": 0,
+                "activities": 0
+            },
+            "3": {
+                "notes": 0,
+                "comments": 0,
+                "exercises": 0,
+                "activities": 0
+            },
+            "4": {
+                "notes": 0,
+                "comments": 0,
+                "exercises": 0,
+                "activities": 0
+            },
+            "5": {
+                "notes": 0,
+                "comments": 0,
+                "exercises": 0,
+                "activities": 0
+            },
+            "6": {
+                "notes": 0,
+                "comments": 0,
+                "exercises": 0,
+                "activities": 0
+            }
+        }
+    },
+    {
+        "name": "JT Ho",
+        "photoPath": "gs://book-club-system.appspot.com/user_image/12_star533422.jpeg",
+        "data": {
+            "1": {
+                "notes": 5,
+                "comments": 0,
+                "exercises": 0,
+                "activities": 0
+            },
+            "2": {
+                "notes": 0,
+                "comments": 0,
+                "exercises": 0,
+                "activities": 0
+            },
+            "3": {
+                "notes": 0,
+                "comments": 0,
+                "exercises": 0,
+                "activities": 0
+            },
+            "4": {
+                "notes": 0,
+                "comments": 0,
+                "exercises": 0,
+                "activities": 0
+            },
+            "5": {
+                "notes": 0,
+                "comments": 0,
+                "exercises": 0,
+                "activities": 0
+            },
+            "6": {
+                "notes": 0,
+                "comments": 0,
+                "exercises": 0,
+                "activities": 0
+            }
+        }
+    },
+    {
+        "name": "rororo",
+        "photoPath": "gs://book-club-system.appspot.com/user_image/2_rororo.jpeg",
+        "data": {
+            "1": {
+                "notes": 5,
+                "comments": 0,
+                "exercises": 2,
+                "activities": 0
+            },
+            "2": {
+                "notes": 0,
+                "comments": 0,
+                "exercises": 0,
+                "activities": 0
+            },
+            "3": {
+                "notes": 0,
+                "comments": 0,
+                "exercises": 0,
+                "activities": 0
+            },
+            "4": {
+                "notes": 0,
+                "comments": 0,
+                "exercises": 0,
+                "activities": 0
+            },
+            "5": {
+                "notes": 0,
+                "comments": 0,
+                "exercises": 0,
+                "activities": 0
+            },
+            "6": {
+                "notes": 0,
+                "comments": 0,
+                "exercises": 0,
+                "activities": 0
+            }
+        }
+    },
+];
